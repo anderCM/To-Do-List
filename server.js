ebe7088b575f3eae0e826cec5355a544a6577444
anderCM2 <ginoander_5@hotmail.com>
2023-01-17 11:14:00 -0500

    Linterns added and Set up webpack

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
+
+// Serve the files on port 8080.
+app.listen(8080, function () {
+  console.log('App running on port 8080!\n');
+});
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+   publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
